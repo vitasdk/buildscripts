c66adebe578686e55dc36a309f16a549e1aafc36
codestation <codestation404@gmail.com>
2016-10-01 19:27:33 -0400

    feat: simplify vita-headers installation.

--- cmake/install_files.cmake
+++ cmake/install_files.cmake
@@ -1,4 +1,5 @@
-file(INSTALL ${SOURCE} DESTINATION ${DEST}
+# do not use quotes when passing the glob pattern
+file(GLOB FILE_LIST ${GLOB_PATTERN})
+file(INSTALL ${FILE_LIST} DESTINATION "${INSTALL_DIR}"
     PATTERN ".git" EXCLUDE
-    PATTERN "../.gitignore" EXCLUDE
-    )
+    PATTERN ".gitignore" EXCLUDE)
