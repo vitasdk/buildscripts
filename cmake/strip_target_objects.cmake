8ca1ed610e4e0dbc11ac2aa4224af7095d28865c
codestation <codestation404@gmail.com>
2016-10-08 20:29:36 -0400

    Use objcopy from the binutils host.

--- cmake/strip_target_objects.cmake
+++ cmake/strip_target_objects.cmake
@@ -1,8 +1,13 @@
 # do not use quotes when passing the glob pattern
 file(GLOB obj_libs ${PATTERN_GLOB})
 
+# set default strip command
+if(NOT "${CMAKE_OBJCOPY}")
+    set(CMAKE_OBJCOPY "objcopy")
+endif()
+
 foreach(obj ${obj_libs})
-    execute_process(COMMAND ${OBJCOPY_COMMAND} -R .comment -R .note
+    execute_process(COMMAND ${CMAKE_OBJCOPY} -R .comment -R .note
         -R .debug_info -R .debug_aranges -R .debug_pubnames
         -R .debug_line -R .debug_pubtypes -R .debug_abbrev
         -R .debug_str -R .debug_ranges -R .debug_loc ${obj})
