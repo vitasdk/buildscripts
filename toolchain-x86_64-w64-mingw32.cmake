04c4bb4570c73609791452c5f5b7160a95173ed6
Yifan Lu <me@yifanlu.com>
2016-12-07 20:35:26 -0800

    Added newlib as a dependency to gcc-complete because it require headers Commented out mingw path because it is not needed

--- toolchain-x86_64-w64-mingw32.cmake
+++ toolchain-x86_64-w64-mingw32.cmake
@@ -10,7 +10,7 @@ SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32/sys-root/mingw)
+#SET(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32/sys-root/mingw)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
