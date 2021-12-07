be53e944c00b04f039ff495a1a2b662589b044d3
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-12-07 14:46:28 +0900

    Add psp2rela to tools

--- cmake/create_version.cmake
+++ cmake/create_version.cmake
@@ -1,6 +1,7 @@
 string(TIMESTAMP BUILD_DATE "%Y-%m-%d %H:%M:%S" UTC)
 
 file(READ ${INPUT_DIR}/vita-toolchain-version.txt _toolchain_sha1)
+file(READ ${INPUT_DIR}/psp2rela-version.txt _psp2rela_sha1)
 file(READ ${INPUT_DIR}/vita-headers-version.txt _headers_sha1)
 file(READ ${INPUT_DIR}/newlib-version.txt _newlib_sha1)
 file(READ ${INPUT_DIR}/pthread-embedded-version.txt _pthread_sha1)
@@ -12,3 +13,4 @@ file(APPEND ${OUTPUT_FILE} "pthread-embedded  ${_pthread_sha1}")
 file(APPEND ${OUTPUT_FILE} "samples           ${_samples_sha1}")
 file(APPEND ${OUTPUT_FILE} "vita-headers      ${_headers_sha1}")
 file(APPEND ${OUTPUT_FILE} "vita-toolchain    ${_toolchain_sha1}")
+file(APPEND ${OUTPUT_FILE} "psp2rela          ${_psp2rela_sha1}")
