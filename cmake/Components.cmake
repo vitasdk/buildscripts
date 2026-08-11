59a2679aada0bf8df2676623072a156c174c999f
Francisco José García García <frangarcj@gmail.com>
2026-08-11 13:57:48 +0200

    Components: update HEADERS_TAG to 2f499a02e

--- cmake/Components.cmake
+++ cmake/Components.cmake
@@ -71,7 +71,7 @@ set(VITA_MAKEPKG_REPOSITORY https://github.com/vitasdk/vita-makepkg.git
 
 set(NEWLIB_TAG 892f530fa7d696dbca74abfb8fccfec7d21d269d CACHE STRING "newlib branch, commit id or tag")
 set(SAMPLES_TAG fe8fbef570f3280586c0c20157146e3faefb2181 CACHE STRING "samples branch, commit id or tag")
-set(HEADERS_TAG 341895bb592ea663063f641f20a48804f3c7afcb CACHE STRING "vita-headers branch, commit id or tag")
+set(HEADERS_TAG 2f499a02e335dc9e678f4680eb0302264e4603c0 CACHE STRING "vita-headers branch, commit id or tag")
 set(TOOLCHAIN_TAG c527abce028df33f5281e9ed4994c25fcef53c7d CACHE STRING "vita-toolchain branch, commit id or tag")
 set(PTHREAD_TAG 610934f4841f02a42f19f0e8b0f01bb38b375dfc CACHE STRING "pthread-embedded branch, commit id or tag")
 set(VDPM_TAG v0.1.0-rc1 CACHE STRING "vdpm branch, commit id or tag")
